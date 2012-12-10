24bba6510164e11b49c3197d8fab945c8453cc9f
francescas <francescas@wolfram.com>
2012-12-10 13:16:49 -0700

    Update include/MVC/Controller/entry_point_registry.php
    added entry point for filter_subpanel

--- include/MVC/Controller/entry_point_registry.php
+++ include/MVC/Controller/entry_point_registry.php
@@ -71,5 +71,6 @@ $entry_point_registry = array(
     'getYUIComboFile' => array('file' => 'include/javascript/getYUIComboFile.php', 'auth' => false),
     'UploadFileCheck' => array('file' => 'modules/Configurator/UploadFileCheck.php', 'auth' => true),
     'SAML'=>  array('file' => 'modules/Users/authentication/SAMLAuthenticate/index.php', 'auth' => false),
+    'filter_subpanel' = array('file'=>'include/SubPanel/filter_subpanel.php', 'auth' => true),
 );
 ?>
